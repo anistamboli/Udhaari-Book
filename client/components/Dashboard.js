91bf4875114a22a163cf3716b81500f7a66b8ad0
Suyasha-Pujari <pujarisuyasha@gmail.com>
2021-04-06 11:12:00 +0530

    changes in dashboard

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react'
 import { SearchBar } from 'react-native-elements';
-
 import { StyleSheet, View, Image, TouchableOpacity, Alert } from 'react-native';
+function App() {
+  const [user, setUser] = useState('');
+//const {search } = user;
+/*const onInputChange = e => {
+  setUser({ ...user, [e.target.name]: e.target.value });
+};
+*/
+const SampleFunction=()=>{
 
-export default class Dashboard extends Component {
-  state = {
-    search: '',
-  };
+  Alert.alert("Floating Button Clicked");
 
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
+}
 
-  SampleFunction=()=>{
-
-      Alert.alert("Floating Button Clicked");
-
-  }
-  
-
-  render() {
-    const { search } = this.state;
     return (
-    <View style = {styles.container}>
-      <SearchBar
-        placeholder="Search by Name or RMN..."
-        onChangeText={this.updateSearch}
-        value={search}
-      />
-      
-      <View style={styles.MainContainer}>
+    
+      <View style = {styles.container}>
+        <SearchBar
+          placeholder="Enter Consumer's RMN....."
+          onChange={setUser}
+          value={user}
+        />
+        <View style={styles.MainContainer}>
         
-        <TouchableOpacity activeOpacity={0.5} onPress={this.SampleFunction} style={styles.TouchableOpacityStyle} >
+        <TouchableOpacity activeOpacity={1.5} onPress = {() => SampleFunction()} style={styles.TouchableOpacityStyle} >
 
           <Image source={{uri : 'https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png'}} 
           
@@ -40,41 +33,38 @@ export default class Dashboard extends Component {
         </TouchableOpacity>
 
       </View>
-      
-    </View>
-
+      </View>
+   
     );
   }
-}
-
-const styles = StyleSheet.create({
-
-  MainContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor : '#F5F5F5'
-  },
-
-  TouchableOpacityStyle:{
-
-    position: 'absolute',
-    width: 50,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    right: 30,
-    top: 450,
-  },
-
-  FloatingButtonStyle: {
-
-    resizeMode: 'contain',
-    width: 50,
-    height: 50,
-  },
-  container: {
-    paddingTop: 70,
-    //paddingRight: 100
-  }
-});
+  const styles = StyleSheet.create({
+ 
+    MainContainer: {
+      flex: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor : '#F5F5F5'
+    },
+   
+    TouchableOpacityStyle:{
+   
+      position: 'absolute',
+      width: 50,
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      right: 30,
+      top: 300,
+    },
+   
+    FloatingButtonStyle: {
+   
+      resizeMode: 'contain',
+      width: 50,
+      height: 50,
+    },
+    container:{
+      width : '80%'
+    }
+  });
+export default App;
